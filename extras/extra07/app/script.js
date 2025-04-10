eeeab56e1252a99922e12d0c9c1747b0faeb468c
MarcosBarradas <vini.moraes.1518@gmail.com>
2025-04-10 17:53:27 -0300

    merge com remoto

--- extras/extra07/app/script.js
+++ extras/extra07/app/script.js
@@ -4,11 +4,24 @@
  * @professor Jailson Costa
  * @student Marcos Vinicius
  * @date 10/04/2025
- * @description Verificação de Tipo de Triângulo:
+ * @description solicite ao usuário os comprimentos dos três lados de um triângulo 
+ * e classifique-o como equilátero, isósceles ou escaleno.
 */
 
+let lado1 = parseFloat(prompt("Digite o comprimento do primeiro lado do triângulo:"));
+let lado2 = parseFloat(prompt("Digite o comprimento do segundo lado do triângulo:"));
+let lado3 = parseFloat(prompt("Digite o comprimento do terceiro lado do triângulo:"));
 
-
+switch (true) {
+    case (lado1 == lado2 && lado2 == lado3):
+        alert("O triângulo é equilátero.");
+        break;
+    case (lado1 == lado2 || lado1 == lado3 || lado2 == lado3):
+        alert("O triângulo é isósceles.");
+        break;
+    default:
+        alert("O triângulo é escaleno.");
+}
 
 
 
