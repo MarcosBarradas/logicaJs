50b1c522befa4d36d701cd7d2025097bf681a437
MarcosBarradas <vini.moraes.1518@gmail.com>
2025-04-12 23:09:29 -0300

    finalizei todos os exercícios da pag 46 da manzano. Reforçando minha lógica com laço while. Marcha...

--- manzano/manzano-pag46/pag46-ex5-k/app/script.js
+++ manzano/manzano-pag46/pag46-ex5-k/app/script.js
@@ -1,9 +1,36 @@
 /**
- * @file pag46-ex5-a.js
+ * @file pag46-ex5-k.js
  * @discipline React
  * @professor Jailson Costa
  * @student Marcos Vinicius
  * @date 12/04/2025
- * @description 
+ * @description Calcula a área total de uma residência (sala, cozinha,banheiro,
+ * quartos, área de serviço, quintal,garagem, etc.).
+ * O programa solicita nome, a largura e o comprimento de um determinado
+ * cômodo.
+ * Em seguida apresenta a área do cômodo lido e também uma mensagem solicitando
+ * do usuário a confirmação de continuar calculando novos cômodos.
+ * Caso o usuário responda “NAO”, o programa mostra o total de m2 e finaliza.
 */
 
+//var
+let nomeComodo = "";
+let largura = 0;
+let comprimento = 0;
+let areaComodo = 0;
+let continuar = "";
+let mensagem = "";
+let areaTotal = 0;
+
+//Inicio
+while (continuar !== "n") {
+  nomeComodo = prompt("Informe o nome do cômodo: ");
+  largura = parseFloat(prompt("Informe a largura do cômodo: "));
+  comprimento = parseFloat(prompt("Informe o comprimento do cômodo: "));
+  areaComodo = largura * comprimento;
+  areaTotal += areaComodo;
+  mensagem += `A área do ${nomeComodo} é: ${areaComodo}\n`;
+  continuar = prompt("Deseja continuar? (s/n): ").toLowerCase();
+}
+alert(`${mensagem}A área total da casa é: ${areaTotal}`);
+//FimAlgoritmo
--- manzano/manzano-pag46/pag46-ex5-l/app/script.js
+++ manzano/manzano-pag46/pag46-ex5-l/app/script.js
@@ -1,9 +1,35 @@
 /**
- * @file pag46-ex5-a.js
+ * @file pag46-ex5-l.js
  * @discipline React
  * @professor Jailson Costa
  * @student Marcos Vinicius
  * @date 12/04/2025
  * @description 
 */
+//var
+let numero;
+let maior;
+let menor;
+//Inicio
+numero = parseInt(prompt(`Esse programa recebe vários valores naturais \n
+  e devolve o maior e o menor\n
+  Informe o primeiro número: `));
+maior = numero;
+menor = numero;
+numero = parseInt(prompt(`deseja continuar? \n
+     sim = informe mais um número \n
+     não = informe um número negativo`));
 
+while (numero >= 0) {
+  if (numero > maior) {
+    maior = numero;
+  } else if (numero < menor) {
+    menor = numero;
+  } 
+  numero = parseInt(prompt(`deseja continuar? \n
+     sim = informe mais um número \n
+     não = informe um número negativo`));
+}
+
+alert(`O maior número é: ${maior}\n
+  O menor número é: ${menor}`);
