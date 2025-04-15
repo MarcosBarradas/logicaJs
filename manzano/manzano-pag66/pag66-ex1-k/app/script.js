f220cbfe11cdf7b8ccfb346bc4ad86eebe51bdb1
MarcosBarradas <vini.moraes.1518@gmail.com>
2025-04-15 09:27:38 -0300

    último exercício feito

--- manzano/manzano-pag66/pag66-ex1-k/app/script.js
+++ manzano/manzano-pag66/pag66-ex1-k/app/script.js
@@ -4,10 +4,18 @@
  * @professor Jailson Costa
  * @student Marcos Vinicius
  * @date 12/04/2025
- * @description calcula o fatorial de numeros ímpares de 1 a 10
+ * @description calcula o fatorial de numeros ímpares de 1 a 10 e exibe somatoria deles
  * 
  */ 
         //PENDENTE
+let somatoria;
+for (let i = 1; i < 10; i+=2) {
+  let fatorial = 1;
+  for (let j = 1; j <= i; j++) {
+    fatorial *= j;
+  } console.log(`${i}!: ${fatorial}`);
+  somatoria += fatorial;
+}
 
 
 alert(`fatorial de todos os ímpares de 1 a 10: ${fatorial}`);
